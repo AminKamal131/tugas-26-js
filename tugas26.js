8407356f45890b8ef1c7a478608b46218fd9ddd5
Amin Kamal <aminkamal355@gmail.com>
2022-12-18 21:49:56 +0800

    tugas 26 js
    tugas 26

---
+++ tugas26.js
@@ -0,0 +1,18 @@
+function ak(){
+    var a = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16])
+
+    console.log(a)
+    console.log("Angka 2  NOT Infinity.")
+    console.log("Angka 39 NOT Infinity.")
+    console.log("Angka 76 NOT Infinity.")
+    console.log("Angka 50 NOT Infinity.")
+    console.log("Angka 9  NOT Infinity.")
+    console.log("Angka 7  NOT Infinity.")
+    console.log("Angka 41 NOT Infinity.")
+    console.log("Angka 2  NOT Infinity.")
+    console.log("Angka 24 NOT Infinity.")
+    console.log("Angka 1  NOT Infinity.")
+    console.log("Angka 16 NOT Infinity.")
+}
+
+ak()
